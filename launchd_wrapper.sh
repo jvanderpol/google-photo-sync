1c9811ea89a4979ce489a9fdffccc809af475c15
John VanderPol <john.vanderpol@gmail.com>
2022-09-27 20:10:34 -0500

    Added a timeout

--- launchd_wrapper.sh
+++ launchd_wrapper.sh
@@ -11,6 +11,7 @@
 #     <key>ProgramArguments</key>
 #     <array>
 #       <string>/path/to/launchd_wrapper.sh</string>
+#       <string>600</string>
 #       <string>/path/to/sync.py</string>
 #       <string>--output_dir=/path/to/sync/dir/</string>
 #       <string>--client_config=/path/to/client_config.json</string>
@@ -26,9 +27,37 @@
 #   </dict>
 # </plist>
 
-echo `date`: $@ | tee /dev/stderr
-"$@"
+function pid_is_running() {
+  ps -p $1 | tail -n +2
+}
 
+function wait_for_pid() {
+  end_time=$(( $(date +%s) + $2 ))
+  while [[ ! -z $(pid_is_running $1) ]] && [ $(date +%s) -lt $end_time ]; do
+    sleep 1;
+  done
+  if [[ ! -z $(pid_is_running $1) ]]; then
+    >&2 echo "Process did not complete within $2 seconds, killing pid $1"
+    kill $1
+    sleep 5;
+    if [[ ! -z $(pid_is_running $1) ]]; then
+      >&2 echo "Process did not die after being killed, killing pid $1 via -9"
+      kill -9 $1
+    fi
+  fi
+}
+
+echo `date`: $@ | tee /dev/stderr
+if (( $# < 2 )); then
+  >&2 echo 'usage $0 timeout wrapped_command [arg...]'
+  exit 1
+fi
+timeout=$1
+shift
+"$@" &
+pid=$!
+wait_for_pid $pid $timeout
+wait $pid
 retcode=$?
 if [[ $retcode != 0 ]]; then
   wrapped_command="$@"
